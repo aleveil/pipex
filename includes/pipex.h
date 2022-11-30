ddb4b98f724e73ec9d1ce64ce00a246960499c37
Anthony Leveil <aleveil@z1r1p1.42lyon.fr>
2022-11-30 11:35:13 +0100

    fix libft

--- includes/pipex.h
+++ includes/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aleveil <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: aleveil <aleveil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 15:19:06 by aleveil           #+#    #+#             */
-/*   Updated: 2022/11/29 18:25:24 by aleveil          ###   ########.fr       */
+/*   Updated: 2022/11/30 11:22:46 by aleveil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <stdlib.h>
 # include <sys/types.h>
-# include <wait.h>
+# include <sys/wait.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include "../libft/libft.h"
